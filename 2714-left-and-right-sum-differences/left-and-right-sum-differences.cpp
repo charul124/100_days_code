895f1622c4d17253006f41cfd32e2685eba8b985
Charul Srinivas <88542915+charul124@users.noreply.github.com>
2023-12-16 02:10:07 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (26.25%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    vector<int> leftRightDifference(vector<int>& nums) {
-        vector<int>left;
-        vector<int>right(nums.size(),0);
-        int sum=0;
+     vector<int> leftRightDifference(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n,0);
+        vector<int> right(n,0);
+        vector<int> left;
+        int sum =0;
         left.push_back(0);
-        sum+=nums[0];
-        for(int i=1;i<nums.size();i++){
+        sum += nums[0];
+        
+        for(int i=1;i<n;i++){
             left.push_back(sum);
             sum+=nums[i];
         }
-        sum=0;
-        right[nums.size()-1]=0;
-        sum+=nums[nums.size()-1];
-        for(int i=nums.size()-2;i>=0;i--){
-            right[i]=sum;
-            sum+=nums[i];
+        sum = 0;
+        right[n-1]=0;
+        sum+=nums[n-1];
+        for(int j=n-2;j>=0;j--){
+            right[j]=sum;
+            sum+=nums[j];
         }
-        
-        vector<int>ans(nums.size(),0);
-        for(int i=0;i<nums.size();i++){
-            ans[i]=abs(left[i]-right[i]);
+        for(int k = 0; k<n; k++){
+            ans[k]=abs(left[k]-right[k]); 
         }
         return ans;
     }
