c96c4d34e1c3a47579b1d2611a6bfd5fff0e0108
Charul Srinivas <88542915+charul124@users.noreply.github.com>
2024-01-02 03:40:10 +0530

    Time: 6 ms (13.01%) | Memory: 7 MB (74.97%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     vector<int> sumZero(int n) {
-        vector<int> ans;
-        
-        for(int i=0; i<n/2;i++){
-            ans.push_back(i+1);
+        vector<int> v;
+        if(n%2!=0)
+            v.push_back(0);
+        for(int i=1;i<=n/2;i++){
+            v.push_back(i);
+            v.push_back(i*-(1));
         }
-
-        for(int i=0; i<n/2 ;i++){
-            int num = (ans[i] -(ans[i]*2));
-            ans.push_back(num);
-        }
-
-        if(n%2 != 0){
-            ans.push_back(0);
-        }
-        return ans;
+        return v;
     }
 };
