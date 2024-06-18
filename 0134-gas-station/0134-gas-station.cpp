644786de90373152e763d1f0079528f7d8b62d11
Charul Srinivas <88542915+charul124@users.noreply.github.com>
2024-06-18 22:06:12 +0530

    Time: 118 ms (11.86%), Space: 130.5 MB (81.62%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n = gas.size(); 
-        int total = 0; 
-        int curr = 0; 
+        int deficit = 0;
+        int balance = 0;
         int start = 0;
 
-        for(int i = 0; i < n; i++) {
-            total += gas[i] - cost[i];
-            curr += gas[i] - cost[i];
-            if(curr < 0) {
-                start = i + 1;
-                curr = 0;
+        for(int i = 0; i < gas.size(); i++){
+            balance += gas[i] - cost[i];
+            if(balance < 0){
+                deficit += balance;
+                start = i+1;
+                balance = 0;
             }
         }
-        return total >= 0 ? start : -1;
+        if(deficit + balance >= 0){
+            return start;
+        }
+        else{
+            return -1;
+        } 
     }
 };
