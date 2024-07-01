1e54c062a2ad6f13f8a3d9b0678764df81bc4802
Charul Srinivas <88542915+charul124@users.noreply.github.com>
2024-07-02 01:50:27 +0530

    Time: 6 ms (25.23%), Space: 10.5 MB (83.57%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-        if(arr.size() <= 2){
+        if(arr.size() < 3){
             return false;
         }
         for(int i = 0; i <= arr.size() - 3; i++){
