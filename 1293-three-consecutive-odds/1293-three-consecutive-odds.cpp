c322732244a6276976c979ed2b71703935bd4b3d
Charul Srinivas <88542915+charul124@users.noreply.github.com>
2024-07-02 01:51:04 +0530

    Time: 0 ms (100%), Space: 10.5 MB (83.57%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -7,8 +7,8 @@ public:
         if(arr.size() < 3){
             return false;
         }
-        for(int i = 0; i <= arr.size() - 3; i++){
-            if(arr[i] % 2 != 0 && arr[i+1] % 2 != 0 && arr[i+2] % 2 != 0){
+       for (int i = 0; i <= arr.size() - 3; i++) {
+            if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0) {
                 return true;
             }
         }
