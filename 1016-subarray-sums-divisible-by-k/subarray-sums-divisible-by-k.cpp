25fe1b3ae9bd7fd635c9c6d17b0cfa0057ccc731
Charul Srinivas <88542915+charul124@users.noreply.github.com>
2024-07-17 19:34:53 +0530

    Time: 37 ms (67.43%) | Memory: 35.6 MB (5.43%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -2,15 +2,27 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> sums(k, 0);
-        sums[0]++;
-        int cnt = 0;
-        int currSum = 0;
-        for(int i = 0; i < n; i++) {
-            currSum = (currSum + nums[i] % k + k) % k;
-            cnt += sums[currSum];
-            sums[currSum]++;
+        int prefsum = 0;
+        int ans = 0;
+        unordered_map<int,int> Occ;
+        Occ[0] = 1;
+        for(int i = 0; i < n; i++){
+            prefsum = (prefsum + nums[i]%k + k)%k;
+            ans += Occ[prefsum];
+            Occ[prefsum] ++;
         }
-        return cnt;
+        return ans;
+        // This is my brute force approach
+        // long long count = 0;
+        // for(int i = 0; i < nums.size(); i++){
+        //     long long sum = 0;
+        //     for(int j = i; j < nums.size(); j++){
+        //         sum += nums[j];
+        //         if(sum%k == 0){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
     }
 };
