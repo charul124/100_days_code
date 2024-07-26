c2e97e89b32cbbb0bac77d4e194fef037a96ffe8
Charul Srinivas <88542915+charul124@users.noreply.github.com>
2024-07-27 01:23:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/K-Pangrams/kpangrams.cpp
+++ Difficulty: Easy/K-Pangrams/kpangrams.cpp
@@ -9,20 +9,21 @@ class Solution {
 
     bool kPangram(string str, int k) {
         string s = "";
-        for(auto i :str)
-        {
-            if(i== ' ')continue;
-            else s+=i;
+        for(int i = 0; i < str.length(); i++){
+            if(str[i] == ' ') continue;
+            else{
+                s+=str[i];
+            }
         }
-        
-        if(s.length()<26)return 0;
-        set<char>st;
-        for(int i=0;i<s.length();i++)
-        {
+        if(s.size() < 26) return false;
+        set<char> st;
+        for(int i = 0; i < s.length(); i++){
             st.insert(s[i]);
         }
-        if(st.size()+k >=26 || st.size()>=26)return 1;
-        return 0;
+        
+        if(st.size() + k >= 26 || st.size() >= 26) return true;
+        
+        return false;
     }
 };
 
